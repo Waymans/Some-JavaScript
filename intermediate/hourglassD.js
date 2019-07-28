eeb62683def8a3bf5a55ae929d558976d006fe53
Waylan Hedine <hedin090@umn.edu>
2019-07-28 18:09:32 -0500

    fixed styling

--- intermediate/hourglassD.js
+++ intermediate/hourglassD.js
@@ -1,27 +1,38 @@
-// hourglass for 1D array
-var arr = [
-1, 1, 1, 0, 0, 0,
-0, 1, 0, 0, 0, 0,
-1, 1, 1, 0, 0, 0,
-0, 0, 2, 4, 4, 0,
-0, 0, 0, 2, 0, 0,
-0, 0, 1, 2, 4, 0 ]; // res = 19
-var arr2 = [
-1, 1, 1, 0, 0, 0,
-0, 1, 0, 0, 0, 0,
-1, 1, 1, 0, 0, 0,
-0, 9, 2, -4, -4, 0,
-0, 0, 0, -2, 0, 0,
-0, 0, -1, -2, -4, 0 ]; // res = 13
+// finds max hourglass value in given 36-digit 1D array
+// array takes 3-1-3 format, e.g
+// x,x,x
+//  ,x, 
+// x,x,x
 function hourglassD(arr) {
-  var amt = [];
-  function loop(a,b) {
-  	for (let i=a; i<b; i++) {
-      var total = arr[i]+arr[i+1]+arr[i+2]+arr[i+7]+arr[i+12]+arr[i+13]+arr[i+14];
-      amt.push(total);
-  	}
-  }
-  loop(0,4);loop(6,10);loop(12,16);loop(18,22);
-  return Math.max(...amt);
+	let amt = [];
+	
+	// finds the sum of the hourglass
+	function loop(start, end) {
+		for (let i = start; i < end; i++) {
+			let total = 
+				arr[i] + arr[i + 1] + arr[i + 2] + 
+				arr[i + 7] + 
+				arr[i + 12] + arr[i + 13] + arr[i + 14];
+			amt.push(total);
+		}
+	}
+	
+	// when to start/end the loops
+	for (let i = 0; i < 22; i += 6) {
+		loop(i, i+4); // 0,4  6,10  12,16  18,22
+	}
+	
+	return Math.max(...amt);
 }
-console.log(hourglassD(arr1))
+console.log(hourglassD([1, 1, 1, 0, 0, 0, 
+						0, 1, 0, 0, 0, 0, 
+						1, 1, 1, 0, 0, 0, 
+						0, 0, 2, 4, 4, 0, 
+						0, 0, 0, 2, 0, 0, 
+						0, 0, 1, 2, 4, 0 ])); // 19
+console.log(hourglassD([1, 1, 1, 0, 0, 0, 
+						0, 1, 0, 0, 0, 0, 
+						1, 1, 1, 0, 0, 0, 
+						0, 9, 2, -4, -4, 0, 
+						0, 0, 0, -2, 0, 0, 
+						0, 0, -1, -2, -4, 0 ])); // 13
--- intermediate/hourglassDD.js
+++ intermediate/hourglassDD.js
@@ -1,22 +1,32 @@
-// hourglass for 2D array
-var arr = [
-  [-9, -9, -9, 1, 1, 1],
-  [0, -9, 0, 4, 3, 2],
-  [-9, -9, -9, 1, 2, 3],
-  [0, 0, 8, 6, 6, 0],
-  [0, 0, 0, -2, 0, 0],
-  [0, 0, 1, 2, 4, 0] ]; // res = 28
+// finds max hourglass value in given 36-digit 2D array
+// array takes 3-1-3 format, e.g
+// x,x,x
+//  ,x, 
+// x,x,x
 function hourglassDD(arr) {
-  var amt = [];
-  function loop(j) {
-  	for (let i=0; i<4; i++) {
-      var total = arr[j][i]+arr[j][i+1]+arr[j][i+2]+
-          		  arr[j+1][i+1]+
-          		  arr[j+2][i]+arr[j+2][i+1]+arr[j+2][i+2];
-      amt.push(total);
-  	}
-  }
-  loop(0);loop(1);loop(2);loop(3);
-  return Math.max(...amt);
+	let amt = [];
+	
+	// find sum of hourglass figure
+	function loop(j) {
+		for (let i = 0; i < 4; i++) {
+			let total = 
+				arr[j][i] + arr[j][i + 1] + arr[j][i + 2] + 
+				arr[j + 1][i + 1] + 
+				arr[j + 2][i] + arr[j + 2][i + 1] + arr[j + 2][i + 2];
+			amt.push(total);
+		}
+	}
+	
+	// when to start/end loops
+	for (let i = 0; i < 4; i++) {
+		loop(i);
+	}
+	return Math.max(...amt);
 }
-console.log(hourglassDD(arr))
+console.log(hourglassDD([
+	[-9, -9, -9, 1, 1, 1], 
+	[0, -9, 0, 4, 3, 2], 
+	[-9, -9, -9, 1, 2, 3], 
+	[0, 0, 8, 6, 6, 0], 
+	[0, 0, 0, -2, 0, 0], 
+	[0, 0, 1, 2, 4, 0] ])); // 28
